df7518461d24717d802b47120229bbb41933c5b1
venkat-ka <34773521+venkat-ka@users.noreply.github.com>
2022-08-13 16:46:43 +0530

    Sixth update
    Sixth update

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/CurrencyExchangeController.java
@@ -30,7 +30,7 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/home")
 	public String home() {
-		return "Fifth Updated the Welcome to Devops Docker";
+		return "Sixth Updated the Welcome to Devops Docker";
 	}
 
 	//http://localhost:8000/currency-exchange/from/USD/to/INR
