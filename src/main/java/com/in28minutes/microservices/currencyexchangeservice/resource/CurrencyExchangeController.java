3c09db1fc27c036174655b45280a90a6f50dba5a
venkat-ka <34773521+venkat-ka@users.noreply.github.com>
2022-08-14 20:59:29 +0530

    Ninth reversed
    Ninth reversed impl

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/CurrencyExchangeController.java
@@ -30,7 +30,7 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/home")
 	public String home() {
-		return "Eigth Updated the Welcome to Devops Docker";
+		return "Ninth Updated the Welcome to Devops Docker";
 	}
 
 	//http://localhost:8000/currency-exchange/from/USD/to/INR
